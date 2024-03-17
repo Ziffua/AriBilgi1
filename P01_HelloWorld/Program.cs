21f9f1e7a7a143a0d20720010757b884e9bf5b35
Zifual <zfalbayrak@gmail.com>
2024-03-18 02:55:10 +0300

    p01 and p02

---
+++ P01_HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace P01_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string msg1 = "this is the message";
+
+            Console.WriteLine("\n\n"+msg1);
+        }
+    }
+}
---
+++ P02_VariableWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P02_VariableWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            int f = 10, g, h = 5;
+
+            var x = 15;
+            var y = 'A';
+            var z = "Yoo";
+            var t = h;
+
+            int toplam = f + h + x;
+
+            Console.WriteLine(z+toplam.ToString()+toplam);
+            Console.WriteLine(toplam+toplam+"\n"+toplam.ToString()+toplam.ToString());
+
+
+        }
+    }
+}
